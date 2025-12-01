58f5bd41dc54e541d3cad902626e1821979761c6
ali-khalife04 <ali.khalife04@outlook.com>
2025-12-01 14:38:42 +0100

    Bigger Font

--- frontend/src/components/LabelBadge.tsx
+++ frontend/src/components/LabelBadge.tsx
@@ -48,7 +48,7 @@ const sizeConfig: Record<
   },
   lg: {
     container: "",
-    labelFontSize: 10, // increase if you want it even bigger
+    labelFontSize: 100, // increase if you want it even bigger
     descFontSize: 20,
   },
 };
